7de6df16f1b8612abfe6d8581a2993c42fc08712
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-05-17 15:34:14 +0700

    หน้าต่างแบบประเมินความสนใจของนักศึกษา

--- app/containers/StudentsSystem/InterestQuestion.js
+++ app/containers/StudentsSystem/InterestQuestion.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
 import { Helmet } from 'react-helmet';
+import FormControl from '@mui/material/FormControl';
+import Button from '@mui/material/Button';
 import brand from 'dan-api/dummy/brand';
-import { PapperBlock } from 'dan-components';
+import { PapperBlock, RadioGroupVertical } from 'dan-components';
+import questions from '../../api/dummy/question';
 
-function InterestQuestion() {
+export default function InterestQuestion() {
   const title = brand.name + ' - Blank Page';
   const description = brand.desc;
+  const [selectedAnswers, setSelectedAnswers] = useState([]);
+
+  useEffect(() => {
+    questions.map((question) => setSelectedAnswers([[question.id, '-']]));
+  }, []);
+
+  useEffect(() => {
+    console.log(selectedAnswers);
+  }, [selectedAnswers]);
+
+  const handleChange = (Ans) => {
+    const updatedAnswers = [...selectedAnswers];
+    updatedAnswers[Ans[0] - 1] = [Ans[0], Ans[1]];
+    setSelectedAnswers(updatedAnswers);
+  };
+
+  const submit = () => {
+    console.log(selectedAnswers);
+  };
   return (
     <div>
       <Helmet>
@@ -32,12 +55,50 @@ function InterestQuestion() {
         />
       </Helmet>
       <PapperBlock
-        title='Blank Page'
-        desc='Some text description'>
-        Content
+        title='แบบสอบถามความสนใจต่อสายงานด้านต่างๆ'
+        desc='Some text description'
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'center',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'inline-flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              alignContent: 'center',
+              border: '1px solid',
+              width: '50%',
+            }}
+          >
+            <FormControl>
+              {questions.map((question) => (
+                <RadioGroupVertical
+                  key={question.id}
+                  questionsId={question.id}
+                  Labelquestion={question.Labelquestion}
+                  answerLabels={question.answers}
+                  handleChange={handleChange}
+                />
+              ))}
+              <Button
+                variant='contained'
+                color='success'
+                onClick={submit}
+                sx={{
+                  width: '50%',
+                }}
+              >
+                ยืนยัน
+              </Button>
+            </FormControl>
+          </Box>
+        </Box>
       </PapperBlock>
     </div>
   );
 }
-
-export default InterestQuestion;
