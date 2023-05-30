57af990eee5512c020e44449b17f48b96bf68be4
KHAIHWAN <Thod.Thawikhun@outlook.com>
2023-05-30 21:28:17 +0700

    New StudyHistory v.0.2.1

--- app/containers/StudentsSystem/StudyHistory.js
+++ app/containers/StudentsSystem/StudyHistory.js
@@ -10,22 +10,25 @@ import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
-import CardContent from '@mui/material/CardContent';
-import Switch from '@mui/material/Switch';
-
-// @mui Icons button
-// import IconButton from '@mui/material/IconButton';
-import Edit from '@mui/icons-material/Edit';
-import Add from '@mui/icons-material/Add';
-
-// @mui colors
-import { red } from '@mui/material/colors';
-import { alpha } from '@mui/material';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import AboutME from './AboutME';
+import TestTable from './TestTable';
 
 function StudyHistory() {
   const title = brand.name + ' - Blank Page';
   const description = brand.desc;
+
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div>
       <Helmet>
@@ -44,113 +47,96 @@ function StudyHistory() {
             src='https://bit.ly/3W3gF7s'
             sx={{ width: 70, height: 70 }}
           />
-          <Stack spacing={0.5} sx={{ ml: 1, mt: 0.8, flex: 1 }}>
-            <Typography fontWeight={700}>Prachaya Mala</Typography>
+          <Stack sx={{ spacing: 0.5, m: 1, flex: 1 }}>
+            <Typography sx={{ fontWeight: 700 }}>Prachaya Mala</Typography>
             <Typography variant='body2' color='text.secondary'>
-              Nong / Project Management
+              Nong / Project Managemen
             </Typography>
           </Stack>
-          <Stack sx={{ mt: 2.5 }}>
-            <Button
-              variant='outlined'
-              startIcon={<Add sx={{ fontSize: 14 }} />}>
-              Add
-            </Button>
+          <Stack sx={{ m: 1 }}>
+            <Typography
+              sx={{
+                fontWeight: 700,
+                display: 'flex',
+                justifyContent: 'center',
+              }}>
+              29
+            </Typography>
+            <Typography
+              variant='body2'
+              color='text.secondary'
+              sx={{ display: 'flex', justifyContent: 'center' }}>
+              หน่วยกิตที่เทียบโอน
+            </Typography>
           </Stack>
-          <Stack sx={{ mt: 2.5, ml: 1.5 }}>
-            <Button
-              variant='outlined'
-              startIcon={<Edit sx={{ fontSize: 14 }} />}>
-              Edit
-            </Button>
+          <Stack sx={{ m: 1 }}>
+            <Typography
+              sx={{
+                fontWeight: 700,
+                display: 'flex',
+                justifyContent: 'center',
+              }}>
+              22
+            </Typography>
+            <Typography
+              variant='body2'
+              color='text.secondary'
+              sx={{ display: 'flex', justifyContent: 'center' }}>
+              หน่วยกิตที่เรียนแล้ว
+            </Typography>
+          </Stack>
+          <Stack sx={{ m: 1 }}>
+            <Typography
+              sx={{
+                fontWeight: 700,
+                display: 'flex',
+                justifyContent: 'center',
+              }}>
+              79
+            </Typography>
+            <Typography
+              variant='body2'
+              color='text.secondary'
+              sx={{ display: 'flex', justifyContent: 'center' }}>
+              หน่วยกิตที่เหลือ
+            </Typography>
           </Stack>
         </Box>
       </Card>
 
-      <Card sx={{ mt: 2 }}>
-        <Box sx={{ display: 'flex', borderRadius: 1, padding: 1 }}>
-          <Box
-            sx={{
-              borderRadius: 1,
-              backgroundColor: alpha('#ebe6e6', 0.3),
-              width: '33.33%',
-              height: 370,
-              ml: 0.2,
-              p: 1,
-            }}>
-            <Stack spacing={0.5} sx={{ ml: 1, mt: 0.8 }}>
-              <Typography variant='body1' sx={{ color: red[50] }}>
-                Platform Settings
-              </Typography>
-              <Typography variant='body2' color='text.secondary'>
-                ACCOUT
-              </Typography>
-              <CardContent>
-                <Box sx={{ display: 'flex', ml: -3.2, mt: -2 }}>
-                  <Switch defaultChecked />
-                  <Typography variant='body2' sx={{ mt: 1.1 }}>
-                    Email me when someone follows me
-                  </Typography>
-                </Box>
-                <Box sx={{ display: 'flex', ml: -3.2 }}>
-                  <Switch />
-                  <Typography variant='body2' sx={{ mt: 1.1 }}>
-                    Email me when someone answers on my post
-                  </Typography>
-                </Box>
-                <Box sx={{ display: 'flex', ml: -3.2 }}>
-                  <Switch defaultChecked />
-                  <Typography variant='body2' sx={{ mt: 1.1 }}>
-                    Email me when someone mentions me
-                  </Typography>
-                </Box>
-                <Typography
-                  variant='body2'
-                  color='text.secondary'
-                  sx={{ ml: -2.5, mt: 0.5 }}>
-                  APPLICATION
-                </Typography>
-                <Box sx={{ display: 'flex', ml: -3.2 }}>
-                  <Switch />
-                  <Typography variant='body2' sx={{ mt: 1.1 }}>
-                    New launches and projects
-                  </Typography>
-                </Box>{' '}
-                <Box sx={{ display: 'flex', ml: -3.2 }}>
-                  <Switch defaultChecked />
-                  <Typography variant='body2' sx={{ mt: 1.1 }}>
-                    Monthly product updates
-                  </Typography>
-                </Box>{' '}
-                <Box sx={{ display: 'flex', ml: -3.2 }}>
-                  <Switch defaultChecked />
-                  <Typography variant='body2' sx={{ mt: 1.1 }}>
-                    Subscribe to newsletter
-                  </Typography>
-                </Box>
-              </CardContent>
-            </Stack>
+      <Divider sx={{ mt: 2 }} />
+
+      <Box sx={{ width: '100%', typography: 'body1' }}>
+        <TabContext value={value} sx={{ display: 'flex' }}>
+          <Box sx={{ borderColor: 'divider', display: 'flex' }}>
+            <TabList
+              onChange={handleChange}
+              aria-label='lab API tabs example'
+              display='flex'>
+              <Tab label='โครงสร้างหน่วยกิต' value='1' />
+              <Tab label='เกี่ยวกับ' value='2' />
+            </TabList>
+            <Box
+              sx={{
+                display: 'flex',
+                width: '89%',
+                justifyContent: 'end',
+              }}>
+              <Button
+                href='http://localhost:3001/app/updata-studyhistory'
+                variant='text'>
+                เพิ่มประวัติการเรียน
+              </Button>
+            </Box>
           </Box>
-          <Box
-            sx={{
-              borderRadius: 1,
-              backgroundColor: alpha('#ebe6e6', 0.3),
-              width: '33.33%',
-              height: 370,
-              ml: 1.2,
-              p: 1,
-            }}></Box>
-          <Box
-            sx={{
-              borderRadius: 1,
-              backgroundColor: alpha('#ebe6e6', 0.3),
-              width: '33.33%',
-              height: 370,
-              ml: 1.2,
-              p: 1,
-            }}></Box>
-        </Box>
-      </Card>
+          <TabPanel value='1'>
+            <TestTable />
+          </TabPanel>
+          <TabPanel value='2'>
+            <AboutME />
+          </TabPanel>
+        </TabContext>
+      </Box>
     </div>
   );
 }
